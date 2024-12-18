fb11021d60eb884ccebbd245409bb7b10ec7df15
Stefan Bock <143817813+stiebo@users.noreply.github.com>
2024-12-18 08:48:32 +0100

    new Stolen cards view, service, read, write, delete

--- src/main/java/dev/stiebo/app/data/StolenCardRepository.java
+++ src/main/java/dev/stiebo/app/data/StolenCardRepository.java
@@ -1,14 +1,18 @@
 package dev.stiebo.app.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface StolenCardRepository extends JpaRepository<StolenCard,Long> {
-    Boolean existsByNumber (String ip);
-    Optional<StolenCard> findByNumber (String number);
+public interface StolenCardRepository extends JpaRepository<StolenCard, Long>,
+        JpaSpecificationExecutor<StolenCard> {
+    Boolean existsByNumber(String number);
+
+    Optional<StolenCard> findByNumber(String number);
+
     List<StolenCard> findAllByOrderByIdAsc();
 }
