a45e6ab18c4ff7ff33a61be1a460ccb3736a88fb
Stefan Bock <143817813+stiebo@users.noreply.github.com>
2024-12-18 08:06:27 +0100

    add SuspiciousIp GridView, Service, add and delete

--- src/main/java/dev/stiebo/app/data/SuspiciousIpRepository.java
+++ src/main/java/dev/stiebo/app/data/SuspiciousIpRepository.java
@@ -1,14 +1,18 @@
 package dev.stiebo.app.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface SuspiciousIpRepository extends JpaRepository<SuspiciousIp,Long> {
-    Boolean existsByIp (String ip);
-    Optional<SuspiciousIp> findByIp (String ip);
+public interface SuspiciousIpRepository extends JpaRepository<SuspiciousIp, Long>,
+        JpaSpecificationExecutor<SuspiciousIp> {
+    Boolean existsByIp(String ip);
+
+    Optional<SuspiciousIp> findByIp(String ip);
+
     List<SuspiciousIp> findAllByOrderByIdAsc();
 }
