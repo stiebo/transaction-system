e45d9d3670929b6efcbf6b2b84368eb09f1309b2
Stefan Bock <143817813+stiebo@users.noreply.github.com>
2024-12-19 09:07:22 +0100

    add DashboardView

--- src/main/java/dev/stiebo/app/data/TransactionRepository.java
+++ src/main/java/dev/stiebo/app/data/TransactionRepository.java
@@ -1,6 +1,9 @@
 package dev.stiebo.app.data;
 
 import dev.stiebo.app.configuration.Region;
+import dev.stiebo.app.configuration.TransactionStatus;
+import dev.stiebo.app.dtos.DailyTransactionSummary;
+import dev.stiebo.app.dtos.TransactionsByRegion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -11,7 +14,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public interface TransactionRepository extends JpaRepository<Transaction,Long>,
+public interface TransactionRepository extends JpaRepository<Transaction, Long>,
         JpaSpecificationExecutor<Transaction> {
     @Query("SELECT COUNT(DISTINCT t.region) " +
             "FROM Transaction t " +
@@ -28,7 +31,28 @@ public interface TransactionRepository extends JpaRepository<Transaction,Long>,
     Long countDistinctIpsInPeriodExcludingCurrentIp(@Param("startDateTime") LocalDateTime startDateTime,
                                                     @Param("endDateTime") LocalDateTime endDateTime,
                                                     @Param("currentIp") String currentIp);
+
     List<Transaction> findAllByOrderByIdAsc();
+
     List<Transaction> findAllByNumberOrderByIdAsc(String number);
+
+    @Query("SELECT new dev.stiebo.app.dtos.TransactionsByRegion(t.region, COUNT(t)) " +
+            "FROM Transaction t " +
+            "GROUP BY t.region")
+    List<TransactionsByRegion> findTransactionsGroupedByRegion();
+
+    @Query("""
+            SELECT new dev.stiebo.app.dtos.DailyTransactionSummary
+            (DATE_TRUNC('DAY', t.date) as date, t.result, COUNT(*))
+            FROM Transaction t
+            GROUP BY date, t.result
+            ORDER BY date ASC
+            """)
+    List<DailyTransactionSummary> findDailyTransactionSummary();
+
+    Long countByResult(TransactionStatus result);
+
+    @Query("SELECT COUNT(t) FROM Transaction t WHERE t.feedback IN (ALLOWED, MANUAL_PROCESSING, PROHIBITED)")
+    Long countByFeedbackIsNotEmpty();
 
 }
